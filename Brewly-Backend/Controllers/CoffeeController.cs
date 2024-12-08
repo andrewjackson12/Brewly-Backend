f5e8dd5acb1ad539e83d4ebc4e8782c1c3547899
andrewjackson12 <andrewjackson2655@gmail.com>
2024-12-08 14:49:57 -0500

    Add DbConnection, Coffee Controller and initial migration

---
+++ Brewly-Backend/Controllers/CoffeeController.cs
@@ -0,0 +1,25 @@
+﻿using Brewly_Backend.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Brewly_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CoffeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Coffee>>> GetCoffees()
+        {
+            return await _context.Coffee.ToListAsync();
+        }
+    }
+}
---
+++ Brewly-Backend/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Brewly_Backend.Data
+{
+    
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Coffee> Coffee { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
