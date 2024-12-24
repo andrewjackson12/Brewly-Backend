d4efc6c989f1f31083d45fc8323c7b8e6af73d0d
andrewjackson12 <andrewjackson2655@gmail.com>
2024-12-24 12:36:30 -0500

    Add coffee and user models

---
+++ Brewly-Backend/Models/Coffee.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace Brewly_Backend.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string RoastType { get; set; }
+        public string Description { get; set; }
+        public string CountryOfOrigin { get; set; }
+
+    }
+}
