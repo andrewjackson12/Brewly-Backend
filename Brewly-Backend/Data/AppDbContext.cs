4aa6e49c0864f3753d611fc68dbe4c5475c98fce
andrewjackson12 <andrewjackson2655@gmail.com>
2024-12-24 12:37:42 -0500

    Add Db contexts and migrations

--- Brewly-Backend/Data/AppDbContext.cs
+++ Brewly-Backend/Data/AppDbContext.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections.Generic;
+using Brewly_Backend.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Brewly_Backend.Data
 {
-    
+
 
     public class AppDbContext : DbContext
     {
-        public DbSet<Coffee> Coffee { get; set; }
+        
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Coffee> Coffee { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
